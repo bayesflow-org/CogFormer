6dc93f2f5134d6c5421e7574aaa0f8abbfbe4520
jerrymhuang <aca.jerryh@gmail.com>
2025-10-06 15:22:11 -0400

    Include visualization in model family

--- bayesgpt/simulators/model_family.py
+++ bayesgpt/simulators/model_family.py
@@ -3,8 +3,10 @@ from collections.abc import Callable
 
 from .model import Model
 from .context_manager import ContextManager
-from utils.simulator_utils import shifted_softplus
+
 from adapters import Adapter
+from utils.simulator_utils import shifted_softplus
+from viz import visualize_matrix, visualize_matrices, visualize_design_configs
 
 
 class NestedModelFamily:
@@ -148,6 +150,7 @@ class NestedModelFamily:
         discrete_prob: float = 0.5,
         keep_intercept: bool = False,
         flatten_param_outputs: bool = True,
+        visualize: bool = False,
     ) -> list[dict] | dict:
         # Sample num_obs and num_regressors per batch element if not provided
         if num_obs is None:
@@ -204,6 +207,9 @@ class NestedModelFamily:
             list_batch.append(sim_instance)
 
         batch = self.collate(list_batch, flatten_param_outputs=flatten_param_outputs)
+
+        if visualize:
+            self.visualize(batch, intrinsic_params=self.intrinsic_params)
         return batch
 
     def collate(self, list_batch: list[dict], flatten_param_outputs: bool = True) -> dict[str, np.ndarray]:
@@ -287,3 +293,22 @@ class NestedModelFamily:
             "num_regressors": num_regressors_array,
             "position_encodings": position_encodings,
         }
+
+    def visualize(
+        self,
+        batch: dict[str, np.ndarray | list],
+        intrinsic_params: list[str]
+    ):
+        # Design configs (whole)
+        fig_configs = visualize_design_configs(batch["design_configs"], intrinsic_params)
+
+        # Design matrices (per batch)
+        fig_design = visualize_matrices(batch["design_matrices"], title="Design Matrices")
+
+        # Param masks/matrices (per batch)
+        fig_masks = visualize_matrices(batch["param_masks"], title="Parameter Masks")
+        fig_mats = visualize_matrices(batch["param_matrices"], title="Parameter Matrices")
+
+        # Masks (whole)
+        fig_reg = visualize_matrix(batch["regressor_masks"], title="Regressor Masks")
+        fig_disc = visualize_matrix(batch["discrete_masks"], title="Discrete Masks")
