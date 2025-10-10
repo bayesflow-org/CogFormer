541b3379139290eaea6ddc4d4c7918f93957a319
jerrymhuang <aca.jerryh@gmail.com>
2025-10-10 10:47:12 -0400

    Add concatenate to torch adapter

--- bayesgpt/adapters/torch_adapter.py
+++ bayesgpt/adapters/torch_adapter.py
@@ -5,5 +5,9 @@ import numpy as np
 class TorchAdapter:
 
     @staticmethod
+    def to_torch_tensor(x: np.ndarray, copy: bool = False) -> torch.Tensor:
+        return torch.tensor(x) if copy else torch.from_numpy(x)
+
+    @staticmethod
     def concatenate(x: list[torch.Tensor]) -> torch.Tensor:
         return torch.cat(x)
