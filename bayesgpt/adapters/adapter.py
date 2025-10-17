d441c5cca3307a8c2b45cdfb6d1c302e28b0fa0f
jerrymhuang <aca.jerryh@gmail.com>
2025-10-16 20:19:09 -0400

    Include position embedding in Adapter

--- bayesgpt/adapters/adapter.py
+++ bayesgpt/adapters/adapter.py
@@ -68,30 +68,6 @@ class Adapter:
             out = out.astype(dtype, copy=False, casting="same_kind")
         return out
 
-    @staticmethod
-    def encode_position(
-        x,
-        sinusoidal: bool = False,
-        normalize: bool = False
-    ) -> np.ndarray:
-
-        # Make sure that the position is one-dimensional
-        if x.ndim > 1:
-            if x.shape[-1] != 1:
-                raise ValueError("Position must be 1-dimensional.")
-            else:
-                x = x.squeeze()
-
-        # By default, positions are encoded as a linear sequence
-        positions = np.linspace(0, len(x), len(x))
-        if sinusoidal:
-            positions = np.cos(positions)
-        elif normalize:
-            positions = positions / np.max(positions)
-
-        return positions
-
-
     # ----------------- helpers -----------------
     @staticmethod
     def asarray(x) -> np.ndarray:
@@ -166,49 +142,33 @@ class Adapter:
         return indices
 
     @staticmethod
-    def to_torch_tensor(x: np.ndarray, copy: bool = False) -> torch.Tensor:
-        return torch.tensor(x) if copy else torch.from_numpy(x)
+    def build_token_embeddings(
+        regressor_indices: np.ndarray,
+        parameter_indices: np.ndarray,
+        additional_tokens: np.ndarray
+    ):
+        regressor_indices = np.atleast_2d(regressor_indices)
+        parameter_indices = np.atleast_2d(parameter_indices)
+        additional_tokens = np.atleast_2d(additional_tokens)
+
+        # Check if dimensions of indices align
+        num_tokens = regressor_indices.shape[0]
+        assert (
+            parameter_indices.shape[0] == num_tokens and
+            additional_tokens.shape[0] == num_tokens
+        ), "All inputs must have the same number of tokens."
+
+        embeddings = np.concatenate(
+            (regressor_indices, parameter_indices, additional_tokens),
+            axis=-1
+        )
+
+        return embeddings
+
 
     @staticmethod
-    def adapt(samples: dict, intrinsic_params: list[str], device: str | torch.device = torch.device("cuda")) -> dict:
-
-        design_matrices = samples["design_matrices"]
-        batch_size = design_matrices.shape[0]
-        rts = samples["sim_data"]["rts"]
-        choices = samples["sim_data"]["choices"]
-
-        input_data = Adapter.stack([design_matrices, rts, choices], axis=-1)
-        input_data = Adapter.to_torch_tensor(input_data)
-        input_data = input_data.to(torch.float32)
-
-        param_indices = [Adapter.build_parameter_indices(
-            intrinsic_params,
-            num_regressors=samples["max_num_regressors"],
-            num_categories=samples["max_num_categories"],
-        ) for _ in range(batch_size)]
-        param_indices = Adapter.to_torch_tensor(np.array(param_indices))
-        param_indices = param_indices.to(torch.float32)
-
-        regressor_indices = [Adapter.build_regressor_indices(
-            intrinsic_params,
-            num_regressors=samples["max_num_regressors"],
-            num_categories=samples["max_num_categories"],
-        ) for _ in range(batch_size)]
-
-        regressor_indices = Adapter.to_torch_tensor(np.array(regressor_indices)).to(torch.float32)
-
-        param_masks = Adapter.to_torch_tensor(samples["param_masks"]).to(torch.float32)
-        param_matrices = Adapter.to_torch_tensor(samples["param_matrices"]).to(torch.float32)
-
-        out = {
-            "input_data": input_data,
-            "param_indices": param_indices,
-            "regressor_indices": regressor_indices,
-            "param_masks": param_masks,
-            "param_matrices": param_matrices,
-        }
-
-        return Adapter.to_device(out, device)
+    def to_torch_tensor(x: np.ndarray, copy: bool = False) -> torch.Tensor:
+        return torch.tensor(x) if copy else torch.from_numpy(x)
 
     @staticmethod
     def to_device(batch, device):
@@ -221,3 +181,70 @@ class Adapter:
             return batch.to(device)
         else:
             return batch
+
+    @staticmethod
+    def adapt(
+        samples: dict,
+        intrinsic_params: list[str],
+        device: str | torch.device = torch.device("cuda"),
+        additional_tokens: np.ndarray | None = None,
+    ) -> dict:
+        # Fetch inputs from samples and adapt
+        design_matrices = samples["design_matrices"]
+        batch_size = design_matrices.shape[0]
+        rts = samples["sim_data"]["rts"]
+        choices = samples["sim_data"]["choices"]
+
+        input_data = Adapter.stack([design_matrices, rts, choices], axis=-1)
+        input_data = Adapter.to_torch_tensor(input_data).to(torch.float32)
+
+        # Build indices
+        parameter_indices = [Adapter.build_parameter_indices(
+            intrinsic_params,
+            num_regressors=samples["max_num_regressors"],
+            num_categories=samples["max_num_categories"],
+        ) for _ in range(batch_size)]
+        parameter_indices = Adapter.to_torch_tensor(np.array(parameter_indices)).to(torch.float32)
+
+        regressor_indices = [Adapter.build_regressor_indices(
+            intrinsic_params,
+            num_regressors=samples["max_num_regressors"],
+            num_categories=samples["max_num_categories"],
+        ) for _ in range(batch_size)]
+        regressor_indices = Adapter.to_torch_tensor(np.array(regressor_indices)).to(torch.float32)
+
+        # Build tokens
+        num_tokens = parameter_indices[0].shape[0]
+        if additional_tokens is None:
+            additional_tokens = np.zeros((batch_size, num_tokens, 1))
+        else:
+            additional_tokens = np.asarray(additional_tokens)
+            if additional_tokens.ndim == 2:
+                additional_tokens = additional_tokens[..., None]
+
+        token_embeddings_list = []
+        for b in range(batch_size):
+            embeddings = Adapter.build_token_embeddings(
+                regressor_indices=regressor_indices[b],
+                parameter_indices=parameter_indices[b],
+                additional_tokens=additional_tokens[b]
+            )
+            token_embeddings_list.append(embeddings)
+
+        token_embeddings = np.stack(token_embeddings_list, axis=0)
+        token_embeddings = Adapter.to_torch_tensor(token_embeddings).to(torch.float32)
+
+
+        param_masks = Adapter.to_torch_tensor(samples["param_masks"]).to(torch.float32)
+        param_matrices = Adapter.to_torch_tensor(samples["param_matrices"]).to(torch.float32)
+
+        out = {
+            "input_data": input_data,
+            "param_indices": parameter_indices,
+            "regressor_indices": regressor_indices,
+            "param_masks": param_masks,
+            "param_matrices": param_matrices,
+            "token_embeddings": token_embeddings
+        }
+
+        return Adapter.to_device(out, device)
