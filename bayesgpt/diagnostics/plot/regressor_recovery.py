d5558f8c0958e54eab78182b1eaf4f3b028d34c7
jerrymhuang <aca.jerryh@gmail.com>
2025-11-14 11:34:41 -0500

    Add preliminary implementation to regressor-level recovery viz

---
+++ bayesgpt/diagnostics/plot/regressor_recovery.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+from bayesgpt.utils.plot_utils import make_quadratic
+
+
+def regressor_recovery(
+    true: np.ndarray,
+    pred: np.ndarray,
+    params: list[str],
+    param_matrix: np.ndarray,
+    color: str = "#000787",
+    figsize: tuple = None
+):
+
+    num_params, num_regressors = param_matrix.shape
+
+    if figsize is None:
+        figsize = (3 * num_params, 3 * num_regressors)
+
+    f, axarr = plt.subplots(num_params, num_regressors, figsize=figsize, sharex=True, sharey=True)
+
+    for i, ax in enumerate(axarr.flatten()):
+        row = np.floor(i / num_params)
+        col = i % num_params
+
+        if param_matrix[row, col] == 0:
+            sns.scatterplot(x=true[:, i], y=pred[:, i], ax=ax, color=color)
+
+            make_quadratic(ax, true[:, i], pred[:, i])
+
+            ax.grid(True, color="lightgray", linestyle="--", linewidth=0.5, alpha=0.3)
+            ax.set_xlabel("Ground Truth")
+            if i % num_params == 0:
+                ax.set_ylabel("Estimation")
+            ax.set_title(params[i])
+            sns.despine(ax=ax)
+
+    f.tight_layout()
+    return f
