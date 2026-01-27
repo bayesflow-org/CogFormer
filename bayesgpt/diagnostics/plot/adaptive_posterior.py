ddbe8553ccaad19fbfccd7a7d0668a5004f78b60
jerrymhuang <aca.jerryh@gmail.com>
2026-01-27 18:15:56 -0500

    Start implementing adaptive posterior

---
+++ bayesgpt/diagnostics/plot/adaptive_posterior.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from bayesgpt.simulators.context_manager import ContextManager
+
+
+def adaptive_posterior(
+    samples: np.ndarray,
+    design_config: dict,
+    intrinsic_params: list[str],
+    variable_names: list[str],
+    max_num_categories: int,
+    parameter_mask: np.ndarray = None,
+    intercept_color: str = "#4e2a84",
+    main_effect_color: str = "#6969ff",
+    interaction_color: str = "#ff6969",
+    label_fontsize: int = 14,
+    title_fontsize: int = 14,
+    legend_fontsize: int = 14,
+    height: int = 2.5,
+):
+    fig = sns.PairGrid(samples, height=height)
+
+
+
+
+
+    fig.tight_layout()
+    return fig
+
+
+def create_labels(
+    design_config: dict,
+):
+    labels = []
+    for k, v in design_config.items():
+        if k = "1":
+            labels =
+
+
+
+    return labels
