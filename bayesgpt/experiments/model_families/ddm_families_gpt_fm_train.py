ee61c1b02575fec349c99a4479a2a10433aff205
jerrymhuang <aca.jerryh@gmail.com>
2026-01-28 22:11:08 -0500

    Update gpt-fm trainer

--- bayesgpt/experiments/model_families/ddm_families_gpt_fm_train.py
+++ bayesgpt/experiments/model_families/ddm_families_gpt_fm_train.py
@@ -84,7 +84,7 @@ class BayesGPTTrainer:
                 pbar.set_postfix(loss=f"{loss:.4f}", lr=f"{current_lr:.2e}")
                 pbar.update(1)
 
-            if (epoch + 1) % 10 == 0:
+            if (epoch + 1) % 20 == 0:
                 self.val_step(val_config, global_step)
 
             scheduler.step()
@@ -239,8 +239,8 @@ class BayesGPTTrainer:
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--debug", action="store_true", help="Debug mode")
-    parser.add_argument("--epochs", type=int, default=500, help="number of epochs")
-    parser.add_argument("--steps_per_epoch", type=int, default=500, help="number of steps per epoch")
+    parser.add_argument("--epochs", type=int, default=1000, help="number of epochs")
+    parser.add_argument("--steps_per_epoch", type=int, default=200, help="number of steps per epoch")
     parser.add_argument("--train_batch_size", type=int, default=32, help="batch size")
     parser.add_argument("--val_batch_size", type=int, default=200, help="validation batch size")
     parser.add_argument("--lr", type=float, default=2e-4, help="learning rate")
@@ -317,7 +317,7 @@ if __name__ == "__main__":
     }
 
     val_config = {
-        "num_sample_steps": 100,
+        "num_sample_steps": 1000,
         "batch_size": args.val_batch_size,
         "model_family_config": model_family_config,
         "val_sample_config": val_sample_config,
