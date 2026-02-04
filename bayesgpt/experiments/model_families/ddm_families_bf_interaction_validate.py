9de20f86c6fdb59b72eef82b6817e439068c3f05
jerrymhuang <aca.jerryh@gmail.com>
2026-02-04 08:56:31 -0500

    Update BF results

--- bayesgpt/experiments/model_families/ddm_families_bf_interaction_validate.py
+++ bayesgpt/experiments/model_families/ddm_families_bf_interaction_validate.py
@@ -10,9 +10,10 @@ import matplotlib.pyplot as plt
 
 from pathlib import Path
 
-from simulators.model_family import NestedModelFamily
-from simulators.benchmarks.ddms.ddm import DDM
-from simulators.benchmarks.ddms.ddm_priors import ddm_baseline_priors
+from bayesgpt.simulators.model_family import NestedModelFamily
+from bayesgpt.simulators.benchmarks.ddms.ddm import DDM
+from bayesgpt.simulators.benchmarks.ddms.ddm_priors import ddm_priors
+from bayesgpt.simulators.benchmarks.ddms.ddm_link_fun import ddm_link_fun
 
 
 class DDMModelFamilyBF(bf.simulators.Simulator):
@@ -20,7 +21,7 @@ class DDMModelFamilyBF(bf.simulators.Simulator):
     def __init__(self):
         self.model_family = NestedModelFamily(
             model=DDM(),
-            prior_fun=ddm_baseline_priors(),
+            prior_fun=ddm_priors(),
             regressed_params=["v", "a"],
             mask_randomizer_kwargs=dict(
                 free_intrinsics=["v", "a", "tau", "s_v", "s_tau"],
@@ -29,23 +30,30 @@ class DDMModelFamilyBF(bf.simulators.Simulator):
             )
     )
 
-    def sample(self, batch_size, num_obs=500, flatten_param_outputs=False, **kwargs):
+    def sample(self, batch_size, num_obs=500, flatten_param_outputs=True, check=False, **kwargs):
+        intrinsic_params = ["v", "a", "tau", "s_v", "s_tau"]
+
+        design_config = {
+            "1": intrinsic_params,
+            "u_1": ["v", "a", "tau", "s_v"],
+            "u_2": ["v", "a", "tau"],
+            "u_1:u_2": ["v", "a"]
+        }
 
         if isinstance(batch_size, tuple):
              batch_size = batch_size[0]
 
         sample_kwargs = {
-            'min_num_regressors': 2,
+            "design_config": design_config,
             "max_num_regressors": 2,
             "max_num_categories": 2,
-            "fixed_config": True,
-            "add_interaction": True,
         }
 
         samples = self.model_family.batch_sample(
             batch_size=batch_size,
             num_obs=num_obs,
             flatten_param_outputs=flatten_param_outputs,
+            link_fun=ddm_link_fun(),
             **sample_kwargs,
             **kwargs
         )
@@ -56,41 +64,46 @@ class DDMModelFamilyBF(bf.simulators.Simulator):
 
         # Special treatment for BF:
         # Trim away zeros from non-regressed params
-        params = params[:, params[0] != 0]
+        if check:
+            f = self.model_family.check_regressed_priors(
+                design_config=design_config,
+                num_obs=num_obs,
+                link_fun=ddm_link_fun()
+            )
 
         return {"rts": rts, "choices": choices, "params": params}
 
-def main(num_samples=300, case="interaction"):
+def main(batch_size=300, num_samples=300, case="interaction"):
     # Define simulator
     ddm_family_simulator = DDMModelFamilyBF()
 
     # define checkpoint filepath
-    checkpoint_path = f"./experiments/checkpoints/ddm_families_bf_{case}/model.keras"
+    checkpoint_path = f"./bayesgpt/experiments/checkpoints/ddm_families_bf_{case}/model.keras"
     approximator = keras.saving.load_model(checkpoint_path)
 
     # Make directories
     param_names = [
         r"$v$", r"$a$", r"$\tau$", r"$s_v$", r"$s_\tau$",
-        r"$u_{1, v}$", r"$u_{1, a}$",
-        r"$u_{2, v}$", r"$u_{2, a}$",
-        r"$u_{1}:u_{2, v}$", r"$u_{1}:u_{2, a}$",
+        r"$u_{1, v}$", r"$u_{1, a}$", r"$u_{1, \tau}$", r"$u_{1, s_v}$", r"$u_{1, s_\tau}$",
+        r"$u_{2, v}$", r"$u_{2, a}$", r"$u_{2, \tau}$", r"$u_{2, s_v}$", r"$u_{2, s_\tau}$",
+        r"$u_1:u_{2, v}$", r"$u_1:u_{2, a}$", r"$u_1:u_{2, \tau}$", r"$u_1:u_{2, s_v}$", r"$u_1:u_{2, s_\tau}$",
     ]
 
-    data_dir = Path("./experiments/data")
-    figures_dir = Path("./experiments/figures")
-    evals_dir = Path("./experiments/evaluations")
+    data_dir = Path("./bayesgpt/experiments/data")
+    figures_dir = Path("./bayesgpt/experiments/figures")
+    evals_dir = Path("./bayesgpt/experiments/evaluations")
     data_dir.mkdir(parents=True, exist_ok=True)
     figures_dir.mkdir(parents=True, exist_ok=True)
     evals_dir.mkdir(parents=True, exist_ok=True)
 
     # Generate validation samples
-    val_sims = ddm_family_simulator.sample(num_samples)
+    val_sims = ddm_family_simulator.sample(batch_size=batch_size)
     post_draws = approximator.sample(conditions=val_sims, num_samples=num_samples)
 
     # Save some of them
-    rts = val_sims["rts"][:10]
-    choices = val_sims["choices"][:10]
-    params = post_draws["params"][:10]
+    rts = val_sims["rts"][:15]
+    choices = val_sims["choices"][:15]
+    params = post_draws["params"][:15]
 
     np.savez(
         data_dir / f"ddm_families_bf_{case}_data.npz",
@@ -131,25 +144,26 @@ def main(num_samples=300, case="interaction"):
         estimates=post_draws,
         targets=val_sims,
         variable_names=param_names,
-        figsize=(12, 9),
+        figsize=(15, 12),
         label_fontsize=14,
-        num_row=3,
-        num_col=4
+        num_row=4,
+        num_col=5
     )
     recovery_path = figures_dir / f"ddm_families_bf_{case}_recovery.pdf"
     recovery.savefig(recovery_path)
     plt.close(recovery)
     logging.info(f"Saved recovery plot to {recovery_path}")
 
-    posterior = bf.diagnostics.plots.pairs_posterior(
-        estimates=post_draws,
-        targets=val_sims,
-        dataset_id=0,
-        variable_names=param_names
-    )
-    posterior_path = figures_dir / f"ddm_families_bf_{case}_posterior.pdf"
-    posterior.savefig(posterior_path)
-    logging.info(f"Saved posterior pairplot to {posterior_path}")
+    for i in range(10):
+        posterior = bf.diagnostics.plots.pairs_posterior(
+            estimates=post_draws,
+            targets=val_sims,
+            dataset_id=i,
+            variable_names=param_names
+        )
+        posterior_path = figures_dir / f"ddm_families_bf_{case}_posterior_{i}.pdf"
+        posterior.savefig(posterior_path)
+        logging.info(f"Saved posterior pairplot {i} to {posterior_path}")
 
 
 if __name__ == '__main__':
