ad59651d08036053d509b2f3b2e9914b625fb338
jerrymhuang <aca.jerryh@gmail.com>
2026-02-05 14:25:48 -0500

    Update DDM priors

--- bayesgpt/simulators/benchmarks/ddms/ddm_priors.py
+++ bayesgpt/simulators/benchmarks/ddms/ddm_priors.py
@@ -4,11 +4,11 @@ from scipy.stats import halfnorm, gamma, norm, beta, lognorm
 
 def ddm_priors():
     return {
-        "v": norm(loc=1., scale=0.4),
+        "v": norm(loc=1., scale=0.8),
         "a": norm(loc=0.25, scale=0.5),
         "tau": norm(loc=-1.2, scale=0.5),
         "s_v": norm(loc=-1.5, scale=1.),
-        "s_tau": norm(loc=-1., scale=1.5)
+        "s_tau": norm(loc=-1.5, scale=0.7)
     }
 
 def ddm_priors1():
