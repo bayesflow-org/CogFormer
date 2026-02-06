2757d9424f2c39708fc90fcd6b157d3c39e4ef20
jerrymhuang <aca.jerryh@gmail.com>
2026-02-06 07:49:11 -0500

    Update gpt-vi trainer

--- bayesgpt/experiments/model_families/ddm_families_gpt_vi_train.py
+++ bayesgpt/experiments/model_families/ddm_families_gpt_vi_train.py
@@ -272,7 +272,7 @@ def parse_args():
     parser.add_argument("--train_batch_size", type=int, default=64, help="batch size")
     parser.add_argument("--val_batch_size", type=int, default=200, help="validation batch size")
     parser.add_argument("--epochs", type=int, default=1000, help="number of epochs")
-    parser.add_argument("--steps_per_epoch", type=int, default=100, help="number of steps per epoch")
+    parser.add_argument("--steps_per_epoch", type=int, default=200, help="number of steps per epoch")
     return parser.parse_args()
 
 if __name__ == "__main__":
