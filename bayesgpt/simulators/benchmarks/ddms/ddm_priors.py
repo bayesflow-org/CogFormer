dfbacea2cc87ca361f0d92dcf45091f8209b93e5
jerrymhuang <aca.jerryh@gmail.com>
2026-03-05 21:48:33 -0500

    Update DDM prior

--- bayesgpt/simulators/benchmarks/ddms/ddm_priors.py
+++ bayesgpt/simulators/benchmarks/ddms/ddm_priors.py
@@ -17,7 +17,7 @@ def ddm_priors():
         "a": norm(loc=0.5, scale=0.5),
         "z": norm(loc=0.0, scale=0.8),
         "tau": norm(loc=-1.2, scale=0.5),
-        "s_v": norm(loc=-1.5, scale=0.4),
+        "s_v": norm(loc=-1.2, scale=1.0),
         "s_tau": norm(loc=-1.5, scale=0.7)
     }
 
